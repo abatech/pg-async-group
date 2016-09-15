81b27b9a4f9b0c9f99ad933128fc84ec7014f518
Daniel Cousens <github@dcousens.com>
2016-09-15 13:53:25 +1000

    add better comment

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var async = require('async')
 
-module.exports = function group (client, queries, callback) {
+module.exports = function transaction (client, queries, callback) {
   client.query('BEGIN', function (err) {
     if (err) return callback(err)
 
     async.parallel(queries, function (err) {
       if (err) {
         return client.query('ROLLBACK', (err2) => {
-          // pass through error to next
+          // preference ROLLBACK error
+          // TODO: combine error?
           callback(err2 || err)
         })
       }
